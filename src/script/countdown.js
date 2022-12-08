aa0b9dcd36452db884dc42df09d594d32540aca8
Alex Junior <alexjr2oficial@gmail.com>
2022-12-08 20:06:10 -0300

    starting the script

---
+++ src/script/countdown.js
@@ -0,0 +1,5 @@
+import { Timer } from "./timer";
+
+const clock = document.getElementById('clock');
+
+clock.innerHTML = `12:00`;
---
+++ src/script/timer.js
@@ -0,0 +1,6 @@
+const Timer = {
+  minutes: 60,
+  secunds: 20,
+}
+
+export { Timer };
