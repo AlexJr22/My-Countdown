cab672780afffa3a16ee587287329224966d6000
Alex Junior <alexjr2oficial@gmail.com>
2022-12-09 21:12:32 -0300

    creating a test function

--- src/script/timer.js
+++ src/script/timer.js
@@ -1,6 +1,14 @@
+
 const Timer = {
+  clock: document.getElementById('clock'),
+
   minutes: 60,
   secunds: 20,
+
+  myFunctionTest() {
+    console.log(this.secunds)
+    console.log(this.minutes)
+  },
 }
 
 export { Timer };
