e4e3b06ab0e7a76b429788598b0d5c2b01177eb8
Alex Junior <alexjr2oficial@gmail.com>
2022-12-10 15:20:36 -0300

    logic error problems

--- src/script/timer.js
+++ src/script/timer.js
@@ -5,9 +5,35 @@ const Timer = {
   minutes: 60,
   secunds: 20,
 
-  init() {
+  init(min=this.minutes, sec=this.secunds) {
+    setInterval(() => {
+      this.timer(min, sec);
+    }, 1000);
+  },
+  
+  timer(min, sec) {
     
-  }
+    console.log('Passou aqui!!')
+
+      if (sec > 0 ) {
+        sec -= 1;
+        
+        if (sec == 0) {
+          min -= 1;
+          sec = 60;
+        };
+
+        
+      };
+      if (sec < 10) {
+        this.clock.innerHTML = `${min}:0${sec}`;
+      };
+      if (min < 10) {
+        this.clock.innerHTML = `0${min}:0${sec}`;
+      };
+      console.log('Passou aqui também!')
+  },
+    
 }
 
 export { Timer };
