d868fe25e1a47802c8dca9cba6a71cc2140c5332
Alex Junior <alexjr2oficial@gmail.com>
2022-12-11 12:12:14 -0300

    creating a function to format the timer

--- src/script/timer.js
+++ src/script/timer.js
@@ -1,12 +1,15 @@
 import { View } from "./View.js";
 
 const Timer = {
-  timer: 9 * 60,
+  timer: 5,
   currentTime: 0,
   interval: null,
 
   timeToMinutes: time => Math.floor(time / 60),
   timeToSecunds: time => time % 60,
+
+  // formatando os minutos e segundos
+  formatTime: time => String(time).padStart(2, "0"),
   
   start() {
     Timer.currentTime = Timer.timer;
@@ -18,8 +21,8 @@ const Timer = {
     Timer.currentTime -= 1;
 
     View.render({
-      minutes: `${Timer.timeToMinutes(Timer.currentTime)}`,
-      secunds: `${Timer.timeToSecunds(Timer.currentTime)}`,
+      minutes: `${Timer.formatTime(Timer.timeToMinutes(Timer.currentTime))}`,
+      secunds: `${Timer.formatTime(Timer.timeToSecunds(Timer.currentTime))}`,
     });
 
 
