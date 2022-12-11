abcb4351b88412e5aefa4e12b93b4e9a9fe156af
Alex Junior <alexjr2oficial@gmail.com>
2022-12-11 12:05:05 -0300

    leaving the timer dynamic

--- src/script/timer.js
+++ src/script/timer.js
@@ -1,28 +1,28 @@
 import { View } from "./View.js";
 
 const Timer = {
-  timer: 5,
+  timer: 9 * 60,
   currentTime: 0,
   interval: null,
 
+  timeToMinutes: time => Math.floor(time / 60),
+  timeToSecunds: time => time % 60,
+  
   start() {
     Timer.currentTime = Timer.timer;
     Timer.interval = setInterval(Timer.countdown, 1000);
-
- 
-    
   },
 
   countdown() {
-
-    // console.log(Timer.currentTime);
+    
     Timer.currentTime -= 1;
 
     View.render({
-      minutes: '09',
-      secunds: '18',
-    })
-    
+      minutes: `${Timer.timeToMinutes(Timer.currentTime)}`,
+      secunds: `${Timer.timeToSecunds(Timer.currentTime)}`,
+    });
+
+
     if (Timer.currentTime === 0) {
       clearInterval(Timer.interval);
       Timer.start();
