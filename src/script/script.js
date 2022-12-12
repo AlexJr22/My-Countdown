c02c2749689e722d26f895b96cfc5fc9af665a71
Alex Junior <alexjr2oficial@gmail.com>
2022-12-12 20:29:08 -0300

    refactoring the code

--- src/script/script.js
+++ src/script/script.js
@@ -1,3 +1,36 @@
-import { Timer } from "./timer.js";
 
-Timer.start();
+const Tempo = {
+  tempo: 7 * 60,
+  interval: null,
+
+  tempoEmMinutos: tempo => Math.floor(tempo / 60),
+  tempoEmSegundos: tempo => tempo % 60,
+
+  iniciar() {
+    Tempo.interval = setInterval(Tempo.cronometro, 1000);
+  },
+
+  cronometro() {
+    
+    Tempo.render();
+    
+    Tempo.tempo -= 1;
+    
+    if(Tempo.tempo === -1) {
+      clearInterval(Tempo.interval);
+      Tempo.iniciar();
+      Tempo.tempo = 5;
+    };
+
+  },
+
+  render() {
+    document.body.innerHTML = `<div id="clock">${
+      String(Tempo.tempoEmMinutos(Tempo.tempo)).padStart(2, "0")
+    }:${
+      String(Tempo.tempoEmSegundos(Tempo.tempo)).padStart(2, "0")
+    }</div>`;
+  },
+};
+
+Tempo.iniciar();
